1d1ac714b34034bb359646810e16addee5414f33
sangwoo <sagnwoo0560@gmail.com>
2023-09-04 19:37:37 +0900

    채팅기능 추가

--- src/page/ChatMessage.tsx
+++ src/page/ChatMessage.tsx
@@ -45,6 +45,8 @@ export function ChatMessage() {
             console.log('소켓 chatMessage 실행');
 
             setMessageList(data);
+            console.log('state.user_id', state.user_id);
+
             socketAtom!.emit('readChatRoomList', state.user_id);
             // if (data && Array.isArray(data)) {
             //     const modifiedData = data.map((message: GetMessage) => {
