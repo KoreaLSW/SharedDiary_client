b78cf35a0d5f11cd2c5f702a433c742d30cb72be
sangwoo <sagnwoo0560@gmail.com>
2023-09-06 00:15:07 +0900

    http 코드 수정

--- src/api/diary.ts
+++ src/api/diary.ts
@@ -6,7 +6,6 @@ import {
     DeleteDiary,
     SelectDiaryMonth,
 } from '../type/diary';
-import HttpClient from '../network/http';
 
 const headers = {
     'Content-Type': 'multipart/form-data',
@@ -18,15 +17,13 @@ const client = axios.create({
     withCredentials: true, // withCredentials 설정
 });
 
-const http = new HttpClient(process.env.REACT_APP_BASE_URL!);
-
 export async function getAll(
     userId: string,
     page: number,
     offset: number
 ): Promise<any> {
     const url: string = '/diary/all';
-    return http.client
+    return client
         .get(url, {
             params: { userId, page, offset },
         })
@@ -35,9 +32,7 @@ export async function getAll(
 
 export async function getByUserId(userId: string): Promise<any> {
     const url: string = '/diary';
-    return http.client
-        .get(url, { params: { userId } })
-        .then((result) => result);
+    return client.get(url, { params: { userId } }).then((result) => result);
 }
 
 export async function getByUserIdPage(
@@ -46,14 +41,14 @@ export async function getByUserIdPage(
     offset: number
 ): Promise<any> {
     const url: string = '/diary/page';
-    return http.client
+    return client
         .get(url, { params: { userId, page, offset } })
         .then((result) => result);
 }
 
 export async function getByMonthHome(data: SelectDiaryMonth): Promise<any> {
     const url: string = '/diary/month/home';
-    return http.client
+    return client
         .get(url, {
             params: { user_id: data.user_id, month: data.month },
         })
@@ -66,7 +61,7 @@ export async function getByMonth(
     offset: number
 ): Promise<any> {
     const url: string = '/diary/month/page';
-    return http.client
+    return client
         .get(url, {
             params: { user_id: data.user_id, month: data.month, page, offset },
         })
@@ -75,17 +70,17 @@ export async function getByMonth(
 
 export async function create(diary: FormData): Promise<any> {
     const url: string = '/diary';
-    return http.client.post(url, diary, headers).then((result) => result);
+    return client.post(url, diary, headers).then((result) => result);
 }
 
 export async function update(diary: UpdateDiary): Promise<any> {
     const url: string = '/diary/' + diary.diary_id;
-    return http.client.put(url, diary.diary, headers).then((result) => result);
+    return client.put(url, diary.diary, headers).then((result) => result);
 }
 
 export async function remove(diary: DeleteDiary): Promise<any> {
     const url: string = '/diary';
-    return http.client
+    return client
         .delete(url, {
             params: { diaryId: diary.diary_id, userId: diary.user_id },
         })
